92159db39527cc4be00ed14f744dfb40b11f3369
aigakusyu <71875416+aigakusyu@users.noreply.github.com>
2020-09-29 04:42:15 +0900

    Update sample_command.py

--- sample_command/sample_command.py
+++ sample_command/sample_command.py
@@ -1,5 +1,16 @@
-def main():
-    print("Hello!!!")
+import pandas as pd
+import urllib.request
+from io import StringIO
 
-if __name__ == '__main__':
-    main()
+url = ""
+
+#csvを読み込む関数
+def read_csv(url):
+    print(url)
+    res = urllib.request.urlopen(url)
+    res = res.read().decode("utf-8")
+    df = pd.read_csv(StringIO( res) )
+    return df
+
+#実行
+read_csv(url)
